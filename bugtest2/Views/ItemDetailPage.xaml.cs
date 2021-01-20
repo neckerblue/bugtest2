882962dc69c4dc0e2e5ea2c787125ea938b7c42f
Mateusz <mateusz.s@agc.com.pl>
2021-01-20 18:59:38 +0100

    Add project files.

---
+++ bugtest2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using bugtest2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace bugtest2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
